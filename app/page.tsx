1adda696c847933b8043a224cc88fb5914951375
Muhammad Ridwan Firdaus <va.rhenka@gmail.com>
2023-06-20 12:37:55 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -50,8 +50,8 @@ export default function Home() {
       <Hero />
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
-          <p>Explore the cars you might like</p>
+          <h1 className="text-4xl font-extrabold">Katalog Mobil</h1>
+          <p>Jelajahi mobil yang mungkin Anda sukai</p>
         </div>
         <div className="home__filters">
           <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
