8f211d4702d0bc18e1749a4544d122769d11ee7a
Muhammad Ridwan Firdaus <va.rhenka@gmail.com>
2023-06-20 14:02:53 +0700

    Update ShowMore.tsx

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -15,7 +15,7 @@ const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
     <div className="w-full flex-center gap-5 mt-10">
       {!isNext && (
         <CustomButton
-          title="Show More"
+          title="Tampilkan Lebih Banyak"
           btnType="button"
           containerStyles="bg-primary-blue rounded-full text-white"
           handleClick={handleNavigation}
