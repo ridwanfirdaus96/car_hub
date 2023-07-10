ee1f2b9686b46d25c49f13b5d38ad8a29c9fa1fb
Muhammad Ridwan Firdaus <va.rhenka@gmail.com>
2023-07-10 15:08:52 +0700

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Temukan, pesan, atau sewa mobil - dengan cepat dan mudah!
+          Saya ganti, pesan, atau sewa mobil - dengan cepat dan mudah!
         </h1>
         <p className="hero__subtitle">
           Sederhanakan pengalaman sewa mobil Anda dengan proses pemesanan kami yang mudah.
